24774f869ac4ede73e0217ad83021a80b27495ff
Jared Lunde <jared.lunde@gmail.com>
2024-08-19 10:32:29 -0700

    fix: add valibot build (#80)

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,11 +4,11 @@ import tsconfig from "./tsconfig.json";
 
 export default defineConfig({
   name: "form-atoms",
-  entry: ["src/index.tsx", "src/zod.ts"],
+  entry: ["src/index.tsx", "src/zod.ts", "src/valibot.ts"],
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
   minify: true,
-  external: ["zod"],
+  external: ["zod", "valibot"],
   target: tsconfig.compilerOptions.target,
 });
